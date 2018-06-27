45485b93696d8ec4b0c9904f8b8267d4529ceafc
Borter Timon <borterti@postupn.ch>
2018-06-27 17:36:41 +0200

    initial spring starter project.

---
+++ eureka-service-discovery/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.github.bbortt.springcloudmicroservicecommunication.eurekaservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
+	}
+}
