96b2d88bc2e766c13f55a27de7b2faa876461dc9
Borter Timon <borterti@postupn.ch>
2018-06-29 08:30:17 +0200

    add zuul entry server.

--- zuul-entry-server/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/zuulentryserver/ZuulEntryServerApplication.java
+++ zuul-entry-server/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/zuulentryserver/ZuulEntryServerApplication.java
@@ -1,12 +1,12 @@
-package com.github.bbortt.springcloudmicroservicecommunication.microservice;
+package com.github.bbortt.springcloudmicroservicecommunication.zuulentryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceApplication {
+public class ZuulEntryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
+		SpringApplication.run(ZuulEntryServerApplication.class, args);
 	}
 }
--- zuul-entry-server/src/test/java/com/github/bbortt/springcloudmicroservicecommunication/zuulentryserver/ZuulEntryServerApplicationTests.java
+++ zuul-entry-server/src/test/java/com/github/bbortt/springcloudmicroservicecommunication/zuulentryserver/ZuulEntryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.bbortt.springcloudmicroservicecommunication.microservice;
+package com.github.bbortt.springcloudmicroservicecommunication.zuulentryserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroserviceApplicationTests {
+public class ZuulEntryServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
