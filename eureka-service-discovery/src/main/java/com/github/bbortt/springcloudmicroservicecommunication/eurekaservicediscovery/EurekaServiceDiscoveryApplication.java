ee9e0a354a58ecf18aae83766b443b84ad030864
Borter Timon <borterti@postupn.ch>
2018-06-29 09:39:04 +0200

    configure zuul and eureka to match service registration.

--- eureka-service-discovery/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
+++ eureka-service-discovery/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.github.bbortt.springcloudmicroservicecommunication.eurekaservicedisc
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceDiscoveryApplication {
 
--- zuul-entry-server/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/zuulentryserver/ZuulEntryServerApplication.java
+++ zuul-entry-server/src/main/java/com/github/bbortt/springcloudmicroservicecommunication/zuulentryserver/ZuulEntryServerApplication.java
@@ -2,11 +2,13 @@ package com.github.bbortt.springcloudmicroservicecommunication.zuulentryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulEntryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulEntryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulEntryServerApplication.class, args);
+    }
 }
